6a8a2dc124d3d5df856dfb157cf8ac3bf1310fb5
ADI <adinh@jiway.lu>
2021-10-18 21:30:03 +0200

    BookService & BokServiceTest : implementing findBooksByTitleIgnoreCase()

--- src/test/java/com/weCode/bookStore/service/BookServiceTest.java
+++ src/test/java/com/weCode/bookStore/service/BookServiceTest.java
@@ -11,10 +11,12 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 
 //
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 //
@@ -53,6 +55,24 @@ public class BookServiceTest {
                 .hasFieldOrPropertyWithValue("title", "Test Dto title")
                 .hasFieldOrPropertyWithValue("description", "Test Dto description")
                 .hasFieldOrPropertyWithValue("releaseYear", 2021);
+    }
+
+    @Test
+    void shouldReturnBooksByBookTitleIgnoreCase() {
+        List<Book> books = new ArrayList<>();
+        Book book = getBook();
+        BookDto bookDto = getBookDto();
+        books.add(book);
+
+        // Mocking BookRepository function
+        when(bookRepository.findBooksByTitleIgnoreCase(anyString())).thenReturn(books);
+
+        // Mocking mapper
+        when(mapper.map(book, BookDto.class)).thenReturn(bookDto);
+
+        List<BookDto> bookDtoList = bookService.getBooksByTitle("test title");
+
+        assertThat(bookDtoList.size()).isEqualTo(1);
     }
 
     private Book getBook() {
