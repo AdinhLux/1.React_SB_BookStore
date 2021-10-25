bbdc89aad7e1b194927efecdb4d41206a68702be
ADI <adinh@jiway.lu>
2021-10-25 21:56:42 +0200

    integrationTest/BookControllerTest.java : setUpHeader()

--- src/test/java/com/weCode/bookStore/integrationTest/BookControllerTest.java
+++ src/test/java/com/weCode/bookStore/integrationTest/BookControllerTest.java
@@ -1,14 +1,20 @@
 package com.weCode.bookStore.integrationTest;
 
 import com.weCode.bookStore.BookStoreApplication;
+import com.weCode.bookStore.config.JwtUtil;
 import com.weCode.bookStore.dto.BookDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.jdbc.Sql;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -25,9 +31,33 @@ public class BookControllerTest {
     @Autowired
     private TestRestTemplate testRestTemplate;
 
+    // Let's inject JwtUtil
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    // Set up Header with token (check UserDetailService.java)
+    void setUpHeader() {
+        String token = jwtUtil.generateToken(new User(
+                "peter@gmail.com", passwordEncoder.encode("dummy password"),
+                new ArrayList<>()
+        ));
+
+        testRestTemplate.getRestTemplate().setInterceptors(
+                Collections.singletonList((((request, body, execution) -> {
+                    request.getHeaders().add("Authorization", "Bearer " + token);
+
+                    return execution.execute(request, body);
+                })))
+        );
+    }
+
     @Test
     @Sql(scripts = {"classpath:insertInitialBookRecordForTest.sql"})
     void shouldReturnBooksWhenBookApiCalled() {
+        setUpHeader();
         BookDto[] listOfBooks = testRestTemplate.getForObject("http://localhost:" + port + "/api/v1/books", BookDto[].class);
         assertThat(listOfBooks).isNotNull();
         assertThat(listOfBooks.length).isEqualTo(18);
@@ -41,6 +71,7 @@ public class BookControllerTest {
     @Test
     @Sql(scripts = {"classpath:insertInitialBookRecordForTest.sql"})
     void shouldReturnBooksWhenBookApiCalledSecondAttempt() {
+        setUpHeader();
         BookDto[] listOfBooks = testRestTemplate.getForObject("http://localhost:" + port + "/api/v1/books", BookDto[].class);
         assertThat(listOfBooks).isNotNull();
         assertThat(listOfBooks.length).isEqualTo(18);
@@ -49,6 +80,7 @@ public class BookControllerTest {
     @Test
     @Sql(scripts = {"classpath:insertInitialBookRecordForTest.sql"})
     void shouldReturnOneBookWhenCalledWithTestTitle() {
+        setUpHeader();
         // Let's try the insensitive case : 'test Title' instead of 'test title'
         BookDto[] listOfBooks = testRestTemplate.getForObject("http://localhost:" + port + "/api/v1/books/test Title", BookDto[].class);
 
